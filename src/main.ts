c74cc1b7dec218f4e5e34e706fef7e428cfb6a15
Kevin Pittevils <kevinpittevils098@hotmail.com>
2017-11-30 01:47:40 +0100

    quicker 1. removed debug code in main

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,4 @@
 import { Server } from "./quicker/server";
-import { Bignum } from "./utilities/bignum";
-import { PacketNumber, ConnectionID } from "./packet/header/base.header";
-
-var pn = PacketNumber.randomPacketNumber();
-console.log(pn.toString());
-var cid = ConnectionID.randomConnectionID();
-var firstHalf = pn.toBuffer().readUInt32BE(0).toString(16); // 4294967295
-var secondHalf =pn.toBuffer().readUInt32BE(4).toString(16); // 4294967295
-console.log("first: " + firstHalf);
-console.log("first: " + secondHalf);
-console.log(cid.toString());
-console.log(pn.toString());
-for(var i = 0; i < 10; i++) {
-    pn = PacketNumber.randomPacketNumber();
-    console.log(pn.toBuffer().toString('hex'));
-}
 
 var server = new Server();
 server.listen('localhost', 10000);
