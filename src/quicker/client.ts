acc8d8bc0fa112b62dbe38731f0fabc881dfe07e
Kevin Pittevils <kevinpittevils098@hotmail.com>
2018-03-29 22:38:02 +0200

    quicker 1. refactored client and server code

--- src/quicker/client.ts
+++ src/quicker/client.ts
@@ -1,46 +1,22 @@
-import { PacketNumber, ConnectionID, Version } from '../packet/header/header.properties';
-import { PacketHandler } from '../utilities/handlers/packet.handler';
-import { Bignum } from '../types/bignum';
-import { Stream, StreamType } from './stream';
-import { Constants } from '../utilities/constants';
-import { ClientInitialPacket } from '../packet/packet/client.initial';
-import { PacketParser, PacketOffset } from '../utilities/parsers/packet.parser';
-import { PacketFactory } from '../utilities/factories/packet.factory';
-import { QTLS, HandshakeState } from '../crypto/qtls';
-import { VersionNegotiationPacket } from '../packet/packet/version.negotiation';
-import { BasePacket } from '../packet/base.packet';
-import { Socket, createSocket, RemoteInfo } from 'dgram';
-import * as fs from 'fs';
-import { EndpointType } from '../types/endpoint.type';
-import { ConnectionState, Connection, RemoteInformation, ConnectionEvent } from './connection';
-import { HeaderOffset, HeaderParser } from '../utilities/parsers/header.parser';
-import { HeaderHandler } from '../utilities/handlers/header.handler';
-import { Time, TimeFormat } from '../types/time';
-import { PacketLogging } from '../utilities/logging/packet.logging';
-import { EventEmitter } from 'events';
-import { FrameFactory } from '../utilities/factories/frame.factory';
-import { QuicError } from "./../utilities/errors/connection.error";
-import { ConnectionCloseFrame } from '../frame/close';
-import { ConnectionErrorCodes } from '../utilities/errors/quic.codes';
-import { BaseEncryptedPacket } from '../packet/base.encrypted.packet';
-import { QuicStream } from './quic.stream';
-import { QuickerEvent } from './quicker.event';
-import { TransportParameters } from '../crypto/transport.parameters';
+import {Connection, RemoteInformation, ConnectionEvent} from './connection';
+import {PacketNumber, Version, ConnectionID} from '../packet/header/header.properties';
+import {Constants} from '../utilities/constants';
+import {Bignum} from '../types/bignum';
+import {EndpointType} from '../types/endpoint.type';
+import {QuickerEvent} from './quicker.event';
+import {QuicStream} from './quic.stream';
+import {Stream, StreamType} from './stream';
+import {TransportParameters} from '../crypto/transport.parameters';
+import {Time} from '../types/time';
+import {HeaderOffset} from '../utilities/parsers/header.parser';
+import {PacketOffset} from '../utilities/parsers/packet.parser';
+import {QuickerError} from '../utilities/errors/quicker.error';
+import {QuickerErrorCodes} from '../utilities/errors/quicker.codes';
 import { isIPv6 } from 'net';
-import { QuickerError } from '../utilities/errors/quicker.error';
-import { QuickerErrorCodes } from '../utilities/errors/quicker.codes';
+import { Socket, createSocket, RemoteInfo } from 'dgram';
+import { Endpoint } from './endpoint';
 
-
-export class Client extends EventEmitter {
-
-    private port!: number;
-    private hostname!: string;
-    private options: any;
-
-    private headerParser: HeaderParser;
-    private packetParser: PacketParser;
-    private headerHandler: HeaderHandler;
-    private packetHandler: PacketHandler;
+export class Client extends Endpoint {
 
     private connection!: Connection;
 
@@ -49,10 +25,6 @@ export class Client extends EventEmitter {
 
     private constructor() {
         super();
-        this.headerParser = new HeaderParser();
-        this.headerHandler = new HeaderHandler();
-        this.packetParser = new PacketParser();
-        this.packetHandler = new PacketHandler();
         this.connected = false;
         this.bufferedRequests = [];
     }
@@ -94,7 +66,7 @@ export class Client extends EventEmitter {
         this.connection.setSocket(socket);
         this.setupConnectionEvents();
 
-        socket.on(QuickerEvent.ERROR, (err) => { this.onError(this.connection, err) });
+        socket.on(QuickerEvent.ERROR, (err) => { this.handleError(this.connection, err) });
         socket.on(QuickerEvent.NEW_MESSAGE, (msg, rinfo) => { this.onMessage(msg, rinfo) });
     }
 
@@ -134,14 +106,6 @@ export class Client extends EventEmitter {
         this.connection.sendPackets();
     }
 
-    public getPort(): number {
-        return this.port;
-    }
-
-    public getHostname(): string {
-        return this.hostname;
-    }
-
     public getSession(): Buffer {
         return this.connection.getQuicTLS().getSession();
     }
@@ -176,32 +140,12 @@ export class Client extends EventEmitter {
             if (err instanceof QuickerError && err.getErrorCode() === QuickerErrorCodes.IGNORE_PACKET_ERROR) {
                 return;
             }
-            this.onError(this.connection, err);
+            this.handleError(this.connection, err);
             return;
         }
     }
 
-    private onError(connection: Connection, error: any): any {
-        this.emit(QuickerEvent.ERROR, error);
-        var closeFrame: ConnectionCloseFrame;
-        var packet: BaseEncryptedPacket;
-        if (error instanceof QuicError) {
-            closeFrame = FrameFactory.createConnectionCloseFrame(error.getErrorCode(), error.getPhrase());
-        } else {
-            closeFrame = FrameFactory.createConnectionCloseFrame(ConnectionErrorCodes.INTERNAL_ERROR);
-        }
-        if (connection.getQuicTLS().getHandshakeState() === HandshakeState.COMPLETED) {
-            packet = PacketFactory.createShortHeaderPacket(connection, [closeFrame]);
-        } else {
-            packet = PacketFactory.createHandshakePacket(connection, [closeFrame]);
-        }
-        connection.sendPacket(packet)
-        connection.setState(ConnectionState.Closing);
-    }
-
 }
-
-
 
 interface BufferedRequest {
     request: Buffer,
